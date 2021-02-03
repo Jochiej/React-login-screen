026ea736bec5a9ff18799e47ebece22e6ba88454
Jochem Leunisse <jleunisse@gmail.com>
2021-02-03 21:14:50 +0100

    optimized code commitment

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import "./styles.scss";
 
-
 export const Login = ({ userinfo, setUserInfo }) => {
-  const inputChange = (e) => {
-    if (e.target.name === "username") {
-      setUserInfo({ ...userinfo, username: e.target.value });
-    }
-    if (e.target.name === "password") {
-      setUserInfo({ ...userinfo, password: e.target.value });
-    }
+    const inputChange = (e) => {
+      setUserInfo({ ...userinfo, [e.target.name]: e.target.value });
   };
   const submit = (e) => {
-    //for showing purposes
     e.preventDefault();
     console.log(userinfo);
   };
-
+  
   return (
     <div className="body">
-      <form className="form">
+      <form className="form" onSubmit={(e) => submit(e) }>
         <h1 className="login-title">Login</h1>
         <input
           name="username"
@@ -35,7 +28,7 @@ export const Login = ({ userinfo, setUserInfo }) => {
           onChange={(e) => inputChange(e)}
           placeholder="Password"
         ></input>
-        <button className="button" onClick={(e) => submit(e)}>
+        <button className="button" type="submit">
           Sign in
         </button>
         <p className="p">Lost your password?</p>
